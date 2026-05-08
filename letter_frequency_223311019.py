d2dc41302917b9d6c4a6d681399bd2548076c280
arshadziban <arshadziban031201@gmail.com>
2026-05-08 18:47:07 +0600

    initial commit

---
+++ letter_frequency_223311019.py
@@ -0,0 +1,41 @@
+ciphertext = input("enter the ciphertext: ")
+
+ciphertext = ciphertext.upper()
+
+frequency = {}
+
+for i in range(65, 91):
+    letter = chr(i)
+    frequency[letter] = 0
+
+total_letters = 0
+
+for ch in ciphertext:
+      if ch.isalpha():
+        frequency[ch] += 1
+        total_letters += 1
+
+print("\nletter frequency count")
+
+for letter in frequency:
+    print(letter, ":", frequency[letter])
+
+print("\npercentage frequency")
+
+for letter in frequency:
+    if total_letters > 0:
+        percent = (frequency[letter] / total_letters) * 100
+    else:
+        percent = 0
+
+    print(letter, ":", format(percent, ".2f"), "%")
+
+sorted_letters = sorted(frequency.items(), key=lambda item: item[1], reverse=True)
+
+print("\ntop 5 most frequent letters")
+
+for i in range(5):
+    letter = sorted_letters[i][0]
+    count = sorted_letters[i][1]
+
+    print(str(i + 1) + ".", letter, "-", count, "times")
---
+++ monoalphabetic_cipher_223311019.py
@@ -0,0 +1,38 @@
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+substitution_key = "QWERTYUIOPASDFGHJKLZXCVBNM"
+
+encrypt_table = {}
+decrypt_table = {}
+
+for i in range(26):
+    encrypt_table[alphabet[i]] = substitution_key[i]
+    decrypt_table[substitution_key[i]] = alphabet[i]
+
+print("substitution table")
+
+for letter in alphabet:
+    print(letter, "->", encrypt_table[letter])
+
+plaintext = input("\nenter plaintext: ")
+
+plaintext = plaintext.upper()
+
+ciphertext = ""
+
+for ch in plaintext:
+    if ch.isalpha():
+        ciphertext += encrypt_table[ch]
+    else:
+        ciphertext += ch
+
+print("\nencrypted text:", ciphertext)
+
+decrypted_text = ""
+
+for ch in ciphertext:
+    if ch.isalpha():
+        decrypted_text += decrypt_table[ch]
+    else:
+        decrypted_text += ch
+
+print("decrypted text:", decrypted_text)
